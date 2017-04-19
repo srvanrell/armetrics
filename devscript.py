0ed4371d0870c1d08908aaa075a4c3356c50f09f
srvanrell <srvanrell@gmail.com>
2017-04-19 17:53:33 -0300

    add dev script

---
+++ devscript.py
@@ -0,0 +1,16 @@
+ground = [0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0]
+output = [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0]
+times = range(0)
+
+
+def frames2segments(y_true, y_pred):
+    seg_start = 0
+    seg_end = 0
+
+    # i = 0
+    # while ground[i] ==
+
+    return y_pred == y_true
+
+
+print(frames2segments(ground, output))
