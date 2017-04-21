41e0f00fcb0aa7fe1e0fafb59ea20a4938d6f485
srvanrell <srvanrell@gmail.com>
2017-04-21 18:06:51 -0300

    basic2advanced relabeling

--- devscript.py
+++ devscript.py
@@ -29,12 +29,12 @@ def frames2segments(y_true, y_pred):
     seg_starts = np.append([0], seg_breaks)  # add 0 as the first start
     seg_ends = np.append(seg_breaks, [-1])  # append -1 as the last end
     # Compare segments at their first element to get corresponding labels
-    seg_labels = [label_segment(y_true[i], y_pred[i]) for i in seg_starts]
+    seg_labels = [basic_segment_label(y_true[i], y_pred[i]) for i in seg_starts]
 
     return zip(seg_starts, seg_ends, seg_labels)
 
 
-def label_segment(y_true_seg, y_pred_seg):
+def basic_segment_label(y_true_seg, y_pred_seg):
     """
     Compares y_true_seg with y_pred_seg and returns the corresponding label
     
@@ -52,6 +52,62 @@ def label_segment(y_true_seg, y_pred_seg):
         return "FP"
     else:
         return "TN"
+
+
+def relabel_segments(segments):
+    """
+    Transform labels to:
+    Insertion (Is)
+    Merge (Ms)
+    Overfill (Os). start (Oa), end (Oz)
+    Deletion (Ds)
+    Fragmenting (F)
+    Underfill (Us), start (Ua), end (Uz)
+    
+    :param basic_labeled_segment: 
+    :return: 
+    """
+
+    output = []
+    #FIXME it consider that there are more than one segment
+    # this may not be true
+
+    #TODO add correct
+
+    # First segment relabel
+    aux = ''.join(segments[:2])
+    if aux in ["FPTN", "FPFN"]:
+        output.append("I")  # Insertion
+    elif aux in ["FNTN", "FNFP"]:
+        output.append("D")  # Deletion
+    elif aux in ["FPTP"]:
+        output.append("Oa")  # start Overfill
+    elif aux in ["FNTP"]:
+        output.append("Ua")  # start Underfill
+
+    for i in range(1, len(segments)):
+        aux = ''.join(segments[i-1:i+1])
+        if aux in ["TPFPTP"]:
+            output.append("M")  # Merge
+        elif aux in ["TNFPTN", "FNFPTN", "TNFPFN", "FNFPFN"]:
+            output.append("I")  # Insertion
+        elif aux in ["TNFNTN", "FPFNTN", "TNFNFP", "FPFNFP"]:
+            output.append("D")  # Deletion
+        elif aux in ["TPFNTP"]:
+            output.append("F")  # Fragmentation
+        #TODO add over and under fill
+
+    # TODO add last segment relabel
+
+    # for i, seg in enumerate(segments):
+    #
+    #
+    #
+    #
+    #     segment
+
+
+    return output
 
 for start, end, label in frames2segments(ground, output):
     print(start, end, label)
