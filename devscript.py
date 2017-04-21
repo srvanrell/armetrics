3d9c206bc530b194c126cf5b56145b2443ef5e90
srvanrell <srvanrell@gmail.com>
2017-04-21 16:12:58 -0300

    label of single segment

--- devscript.py
+++ devscript.py
@@ -6,7 +6,7 @@ output = np.array([0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0])
 
 def frames2segments(y_true, y_pred):
     """
-    Compute start- and end-indexes of segments.
+    Compute segment boundaries and compare y_true with y_pred.
     
     Segments are derived by comparing y_true with y_pred:
     any change in either y_pred or y_true marks a segment boundary.
@@ -14,20 +14,46 @@ def frames2segments(y_true, y_pred):
       
     :param y_true: array_like
     ground truth
+    
     :param y_pred: array_like
     prediction or classifier output
+    
     :return: array_like, 2 columns
     start- and end-indexes of segments.
     first column corresponds to starts, and second column to ends
     """
+    # start - and end - indexes
+    # of
+    # segments.That is,.
 
-    y_true_breaks = np.flatnonzero(np.diff(y_true))
-    y_pred_breaks = np.flatnonzero(np.diff(y_pred))
-    seg_breaks = np.union1d(y_true_breaks, y_pred_breaks) + 1
-    seg_starts = np.append([0], seg_breaks)
-    seg_ends = np.append(seg_breaks, [-1])
+    y_true_breaks = np.flatnonzero(np.diff(y_true)) + 1  # detect changes in y_true
+    y_pred_breaks = np.flatnonzero(np.diff(y_pred)) + 1  # detect changes in y_pred
+    seg_breaks = np.union1d(y_true_breaks, y_pred_breaks)  # combine both changes
+    seg_starts = np.append([0], seg_breaks)  # add 0 as the first start
+    seg_ends = np.append(seg_breaks, [-1])  # append -1 as the last end
 
     return zip(seg_starts, seg_ends)
+
+
+def label_segment(y_true_seg, y_pred_seg):
+    """
+    Compares y_true_seg with y_pred_seg and returns the corresponding label
+    
+    :param y_true_seg: true value of segment
+    :param y_pred_seg: predicted value of segment
+    :return: label that indicates True Positive, True Negative, False Positive or False Negative.
+    Possible outcomes: "TP", "TN", "FP", or "FN".
+    """
+    # FIXME this should be beautified :/
+    if y_true_seg and y_pred_seg:
+        return "TP"
+    elif y_true_seg and not y_pred_seg:
+        return "FN"
+    elif not y_true_seg and y_pred_seg:
+        return "FP"
+    else:
+        return "TN"
+
 
 for start, end in frames2segments(ground, output):
     print(start, end)
