c4989b18d132991b0f6d2ee133da23710069f81c
Sebastián <srvanrell@gmail.com>
2017-06-04 17:35:24 -0300

    Clean models.py

--- models.py
+++ models.py
@@ -31,17 +31,3 @@ class Segment:
 
     def __repr__(self):
         return str([self.start, self.end, self.label])
-
-    # def add_label(self, label):
-    #     if label not in self.label:
-    #         self.label += label
-    #         if self.label == "MF":
-    #             self.label = "FM"
-    #
-    # def overlap(self, event):
-    #     """
-    #     Return True if given event overlaps with this one (at the beginning or the end).
-    #     :param event:
-    #     :return:
-    #     """
-    #     return self.start <= event.start < self.end or self.start <= event.end < self.end
