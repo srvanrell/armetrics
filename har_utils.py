54ad1e9ca85fdd1eefe35500f12af8f472e3b4e0
srvanrell <srvanrell@gmail.com>
2017-06-13 11:46:04 -0300

    Add function to load labels (like audacity labels)

--- har_utils.py
+++ har_utils.py
@@ -589,3 +589,32 @@ def plot_matching_time(means, labels, errors=None):
     plt.xlabel('Predicted_time/True_time')
 
     plt.show()
+
+standardized_names = {"RUMIA PASTURA": "RUMIA"}
+names_of_interest = ["PASTOREO", "RUMIA"]
+
+
+def load_chewbite(filename):
+    df = pd.read_table(filename, decimal=',', header=None)
+    df.dropna(axis=1, how='all', inplace=True)
+    df.columns = ["start", "end", "label"]
+
+    df = df.round(0)
+    df.label = df.label.str.strip().str.upper()
+    print(df.label.unique())
+    df.label.replace(standardized_names, inplace=True)
+    df = df.loc[df.label.isin(names_of_interest)]
+
+    df[["start", "end"]] = df[["start", "end"]].astype('int64')
+
+    segments = [Segment(start, end, label) for name, (start, end, label) in df.iterrows()]
+    indexes = [np.arange(start, end) for name, (start, end, label) in df.iterrows()]
+
+    frames = segments2frames(segments)
+    indexes = np.concatenate(indexes)
+
+    s = pd.Series(frames, index=indexes)
+
+    s_formatted = s.reindex(np.arange(s.index[-1]), fill_value="")
+
+    return s_formatted
