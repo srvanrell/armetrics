bec2a9f60662fd8a0b1ce9f787ad3235ffbfe3ce
sebastian <srvanrell@gmail.com>
2017-08-21 18:03:25 -0300

    Scaffolding the python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='armetrics',
+      version='0.1',
+      description='Continuous Activity Recognition Metrics based on (Ward et al, 2011)',
+      url='http://github.com/srvanrell/armetrics',
+      author='srvanrell',
+      author_email='srvanrell@gmail.com',
+      license='MIT',
+      packages=['armetrics'],
+      zip_safe=False)
