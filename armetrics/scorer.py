b79ec41cf8ef2893c90f5ef45269db2fd9c45c62
srvanrell <srvanrell@gmail.com>
2019-12-06 15:17:52 -0300

    Move some functions to scorer.py

---
+++ armetrics/scorer.py
@@ -0,0 +1,143 @@
+from armetrics.models import Event, Segment
+
+
+def segment_basic_score(y_true_seg, y_pred_seg):
+    """
+    Compares y_true_seg with y_pred_seg and returns the corresponding label
+
+    :param y_true_seg: true value of segment
+    :param y_pred_seg: predicted value of segment
+    :return: label that indicates True Positive, True Negative, False Positive or False Negative.
+    Possible outcomes: "TP", "TN", "FP", or "FN".
+    """
+    true_vs_pred = {(True, True): "TP",
+                    (True, False): "FN",
+                    (False, True): "FP",
+                    (False, False): "TN"}
+
+    return true_vs_pred[(y_true_seg, y_pred_seg)]
+
+
+def score_segments(basic_scored_segments):
+    """
+    Transform basic labels "TP", "TN", "FN", "FP" to:
+    Correct (C)
+    Correct Null ("")
+    Insertion (I)
+    Merge (M)
+    Overfill (O). starting (Oa), ending (Oz)
+    Deletion (D)
+    Fragmenting (F)
+    Underfill (U), starting (Ua), ending (Uz)
+
+    :param basic_scored_segments:
+    List of basic scores assigned to segments
+    :return:
+    List of advanced scores assigned to segments
+    """
+
+    segments = [Segment(seg.start, seg.end) for seg in basic_scored_segments]
+
+    # First segment relabel
+    aux = ''.join(seg.label for seg in basic_scored_segments[:2])
+    if basic_scored_segments[0].label in ["TP"]:
+        segments[0].label = "C"  # Correct
+    elif basic_scored_segments[0].label in ["TN"]:
+        segments[0].label = ""  # Correct null
+    elif aux in ["FPTN", "FPFN"]:
+        segments[0].label = "I"  # Insertion
+    elif aux in ["FNTN", "FNFP"]:
+        segments[0].label = "D"  # Deletion
+    elif aux in ["FPTP"]:
+        segments[0].label = "Oa"  # starting Overfill
+    elif aux in ["FNTP"]:
+        segments[0].label = "Ua"  # starting Underfill
+
+    # Middle segment relabel
+    for i in range(1, len(basic_scored_segments) - 1):
+        aux = ''.join(seg.label for seg in basic_scored_segments[i - 1:i + 2])
+        if basic_scored_segments[i].label in ["TP"]:
+            segments[i].label = "C"  # Correct
+        elif basic_scored_segments[i].label in ["TN"]:
+            segments[i].label = ""  # Correct null
+        elif aux in ["TPFPTP"]:
+            segments[i].label = "M"  # Merge
+        elif aux in ["TPFNTP"]:
+            segments[i].label = "F"  # Fragmentation
+        elif aux in ["TNFPTN", "FNFPTN", "TNFPFN", "FNFPFN"]:
+            segments[i].label = "I"  # Insertion
+        elif aux in ["TNFNTN", "FPFNTN", "TNFNFP", "FPFNFP"]:
+            segments[i].label = "D"  # Deletion
+        elif aux in ["TNFPTP", "FNFPTP"]:
+            segments[i].label = "Oa"  # starting Overfill
+        elif aux in ["TPFPTN", "TPFPFN"]:
+            segments[i].label = "Oz"  # ending Overfill
+        elif aux in ["TNFNTP", "FPFNTP"]:
+            segments[i].label = "Ua"  # starting Underfill
+        elif aux in ["TPFNTN", "TPFNFP"]:
+            segments[i].label = "Uz"  # ending Underfill
+
+    if len(basic_scored_segments) > 1:
+        # Last segment relabel
+        aux = ''.join(seg.label for seg in basic_scored_segments[-2:])
+        if basic_scored_segments[-1].label in ["TP"]:
+            segments[-1].label = "C"  # Correct
+        elif basic_scored_segments[-1].label in ["TN"]:
+            segments[-1].label = ""  # Correct null
+        elif aux in ["TNFP", "FNFP"]:
+            segments[-1].label = "I"  # Insertion
+        elif aux in ["TNFN", "FPFN"]:
+            segments[-1].label = "D"  # Deletion
+        elif aux in ["TPFP"]:
+            segments[-1].label = "Oa"  # ending Overfill
+        elif aux in ["TPFN"]:
+            segments[-1].label = "Ua"  # ending Underfill
+
+    return segments
+
+
+def score_events(scored_segments, true_events, pred_events):
+    # Create list of true and predicted events with empty labels
+    scored_true_evs = [Event(start, end) for start, end in true_events]
+    scored_pred_evs = [Event(start, end) for start, end in pred_events]
+
+    # True events labeling, first pass (using labeled_segments)
+    for true_ev in scored_true_evs:
+        for seg in scored_segments:
+            if true_ev.start <= seg.start <= true_ev.end:
+                # In the first pass, D and F segments are assigned to true events
+                if seg.label in ["D", "F"]:
+                    true_ev.add_label(seg.label)
+
+    # Pred events labeling, first pass (using labeled_segments)
+    for pred_ev in scored_pred_evs:
+        for seg in scored_segments:
+            if pred_ev.start <= seg.start <= pred_ev.end:
+                # In the first pass, I and M segments are assigned to pred events
+                if seg.label in ["I", "M"]:
+                    pred_ev.add_label(seg.label)
+
+    # True events labeling, second pass (using labels of prediction)
+    for true_ev in scored_true_evs:
+        for pred_ev in scored_pred_evs:
+            if pred_ev.overlap(true_ev):
+                if pred_ev.label in ["M", "FM"]:
+                    true_ev.add_label("M")
+
+    # Pred events labeling, second pass (using labels of ground truth)
+    for pred_ev in scored_pred_evs:
+        for true_ev in scored_true_evs:
+            if true_ev.overlap(pred_ev):
+                if true_ev.label in ["F", "FM"]:
+                    pred_ev.add_label("F")
+
+    # If no label was assigned so far, then it is a correct detected event
+    for true_ev in scored_true_evs:
+        if true_ev.label == "":
+            true_ev.add_label("C")
+
+    for pred_ev in scored_pred_evs:
+        if pred_ev.label == "":
+            pred_ev.add_label("C")
+
+    return scored_true_evs, scored_pred_evs
